cade85325747a29e060faa1df96d60bc66d20e16
Love Lanai <love.lanai@medieinstitutet.se>
2022-12-20 14:27:45 +0100

    fixed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Playground } from "./views/Playground";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/playground" element={<Playground />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/views/Playground.tsx
@@ -0,0 +1,9 @@
+import "./Playground.sass";
+
+export const Playground = () => {
+  return (
+    <div className="playground">
+      <h1 className="test">playground</h1>
+    </div>
+  );
+};
