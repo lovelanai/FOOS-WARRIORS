91e6d1c146aa1cee0b1f504212319828b8779de0
Love Lanai <love.lanai@medieinstitutet.se>
2022-12-21 15:34:57 +0100

    fixed logo

--- src/assets/logos/logos.ts
+++ src/assets/logos/logos.ts
@@ -1,7 +1,9 @@
 import { ReactComponent as Player } from "@/assets/logos/player.svg";
+import { ReactComponent as BigPlayer } from "@/assets/logos/bigPlayer.svg";
 
 const Logo = {
   Player,
+  BigPlayer,
 };
 
 export default Logo;
