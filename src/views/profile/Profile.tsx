7369ec92e5d17f4c30359c08d16888677bc3aa23
Rosanna Pistone <rosanna@pistone.se>
2022-12-21 17:07:33 +0100

    profilepic skew troubles

--- src/views/profile/Profile.tsx
+++ src/views/profile/Profile.tsx
@@ -4,11 +4,38 @@ import "./Profile.sass";
 
 export const Profile = () => {
   return (
-    <div>
+    <div className="main">
+      <div className="header">
       <Link to="/">
         <ICON.Arrow />
       </Link>
-      Profile
+      <h3>Profile</h3>
+      </div>
+      <div className="img">
+        {/* <img src="https://nationaltoday.com/wp-content/uploads/2022/05/107-Johnny-Depp.jpg"/> */}
+        {/* <div className="img-border"/> */}
+      </div>
+      <div className="info">
+        <h3 className="name">Johnny Depp</h3>
+        <p className="bio">Big baller high roller NPC</p>
+        <div className="stats">
+          <div>
+            <p className="value">26</p>
+            <p className="type">Wins</p>
+            <p>____</p>
+          </div>
+          <div>
+          <p className="value">14</p>
+            <p className="type">Losses</p>
+            <p>____</p>
+          </div>
+          <div>
+          <p className="value">1.4325</p>
+            <p className="type">Ratio</p>
+            <p>____</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
