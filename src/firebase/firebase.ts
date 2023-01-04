d613be1f708f1fb258e1c02bef27438c3ff8de2e
Love Lanai <love.lanai@medieinstitutet.se>
2023-01-04 10:42:06 +0100

    added firebase

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAc2oJIZOs1D5abamMMEUKHG6Z3nPo8TEo",
+  authDomain: "foos-warriors.firebaseapp.com",
+  projectId: "foos-warriors",
+  storageBucket: "foos-warriors.appspot.com",
+  messagingSenderId: "299152234331",
+  appId: "1:299152234331:web:942c85a777d1417a9e6c7e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
