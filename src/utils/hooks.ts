d371805a9f62560c478a02f73798d43fc99dcdce
Love Lanai <love.lanai@medieinstitutet.se>
2023-01-09 13:00:24 +0100

    fixed update profile to & from firebase

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,8 +1,10 @@
+import { useUser } from "./../context/UserContext";
 import { getDocs, collection, doc, getDoc } from "firebase/firestore";
 import { useState, useEffect } from "react";
 import { db } from "../firebase/firebase.config";
 
 export const useFetch = (api: string, id?: string, userId?: string) => {
+  const { fetchUser } = useUser();
   const [response, setResponse] = useState<[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -13,6 +15,7 @@ export const useFetch = (api: string, id?: string, userId?: string) => {
           .then((res) => {
             setResponse(
               res.docs.map((item) => {
+                console.log(item.data());
                 return { ...item.data(), id: item.id };
               }) as any
             );
@@ -21,23 +24,24 @@ export const useFetch = (api: string, id?: string, userId?: string) => {
             setIsLoading(false);
           })
           .catch((error) => {
-            console.log(error);
+            console.log("error", error);
           });
       } else {
         const postById = doc(db, api, id);
         getDoc(postById)
           .then((item) => {
             setResponse({ ...item.data(), id: item.id } as any);
+            console.log(item.data());
           })
           .then(() => {
             setIsLoading(false);
           })
           .catch((error) => {
-            console.log(error);
+            console.log("error", error);
           });
       }
     }
-  }, [api, id, userId]);
+  }, [api, id, userId, fetchUser]);
 
   return { response, isLoading };
 };
