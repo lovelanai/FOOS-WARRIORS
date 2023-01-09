ab19eb4fcb7e9abbe048701da1c0144bde65644f
Rosanna Pistone <rosanna@pistone.se>
2023-01-09 11:51:18 +0100

    added winner view

--- src/components/battlefield-card/BattlefieldCard.tsx
+++ src/components/battlefield-card/BattlefieldCard.tsx
@@ -16,8 +16,6 @@ export const BattlefieldCard = ({
   playerTwo,
   playerThree,
   playerFour,
-  score,
-  winners,
 }: TeamProps) => {
   return (
     <div className="battleCard">
@@ -62,5 +60,100 @@ export const BattlefieldCard = ({
         </div>
       </div>
     </div>
+  );
+};
+
+export const BattlefieldWinnerCard = ({
+  playerOne,
+  playerTwo,
+  playerThree,
+  playerFour,
+  winners
+}: TeamProps) => {
+  console.log(winners)
+  return (
+  <>
+    {winners === "pinkTeam" ? 
+    (
+      <div className="battleWinnerCard">
+<div className="pinkTeam">
+<div
+  className="player"
+  style={{ backgroundImage: `url(${playerOne.img})` }}
+>
+  <div className="overlay">
+    <h4 className="name">{playerOne.name}</h4>
+  </div>
+</div>
+<div
+  className="player"
+  style={{ backgroundImage: `url(${playerTwo.img})` }}
+>
+  <div className="overlay">
+    <h4 className="name">{playerTwo.name}</h4>
+  </div>
+</div>
+</div>
+
+<div className="redTeam-losers">
+<div
+  className="player"
+  style={{ backgroundImage: `url(${playerThree.img})` }}
+>
+  <div className="overlay">
+    <h4 className="name">{playerThree.name}</h4>
+  </div>
+</div>
+<div
+  className="player"
+  style={{ backgroundImage: `url(${playerFour.img})` }}
+>
+  <div className="overlay">
+    <h4 className="name">{playerFour.name}</h4>
+  </div>
+</div>
+</div>
+</div>
+      ):(
+        <div className="battleWinnerCard">
+        <div className="redTeam">
+        <div
+          className="player"
+          style={{ backgroundImage: `url(${playerThree.img})` }}
+        >
+          <div className="overlay">
+            <h4 className="name">{playerThree.name}</h4>
+          </div>
+        </div>
+        <div
+          className="player"
+          style={{ backgroundImage: `url(${playerFour.img})` }}
+        >
+          <div className="overlay">
+            <h4 className="name">{playerFour.name}</h4>
+          </div>
+        </div>
+      </div>
+      <div className="pinkTeam-losers">
+        <div
+          className="player"
+          style={{ backgroundImage: `url(${playerOne.img})` }}
+        >
+          <div className="overlay">
+            <h4 className="name">{playerOne.name}</h4>
+          </div>
+        </div>
+        <div
+          className="player"
+          style={{ backgroundImage: `url(${playerTwo.img})` }}
+        >
+          <div className="overlay">
+            <h4 className="name">{playerTwo.name}</h4>
+          </div>
+        </div>
+      </div>
+      </div>
+      )}
+      </>
   );
 };
