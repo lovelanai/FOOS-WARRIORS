89e7c82de2fa68df65864a88b54bd77f2520e0a5
vacannot <simon.eriksson@hiq.se>
2023-01-10 11:36:04 +0100

    not working zz

--- src/components/leaderboard-card/leaderboardCard.tsx
+++ src/components/leaderboard-card/leaderboardCard.tsx
@@ -4,11 +4,17 @@ import "./leaderboardCard.sass";
 interface leaderboardCard {
   title: string;
   img: string;
-  score: number;
   profileLink?: string;
+  wins: number;
+  losses: number;
 }
 
-export const LeaderboardCard = ({ img, title, score }: leaderboardCard) => {
+export const LeaderboardCard = ({
+  img,
+  title,
+  wins,
+  losses,
+}: leaderboardCard) => {
   return (
     <div className="leaderboardCard">
       <div
@@ -19,7 +25,7 @@ export const LeaderboardCard = ({ img, title, score }: leaderboardCard) => {
       />
 
       <div className="aside">
-        <div className="rank">{score}</div>
+        <div className="rank">{wins / losses}</div>
         <div className="column">
           <h3 className="title">{title}</h3>
           <PrimaryButton title="view profile" profileButton />
