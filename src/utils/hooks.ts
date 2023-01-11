25950caee2825a6dc22543451fd2c4909e935daa
Love Lanai <love.lanai@medieinstitutet.se>
2023-01-11 14:22:58 +0100

    hook for sending notifications is DONE!

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -45,3 +45,45 @@ export const useFetch = (api: string, id?: string, userId?: string) => {
 
   return { response, isLoading };
 };
+
+interface MessageProps {
+  body: string;
+  title: string;
+}
+
+export const sendMessage = async ({ body, title }: MessageProps) => {
+  try {
+    const response = await fetch("https://fcm.googleapis.com/fcm/send", {
+      method: "POST",
+      body: JSON.stringify({
+        to: "f7IhuiPrpWx7ev9yo2xBaX:APA91bHEtDxR1KLytreym1rziQ-9CTBZBp7RS7zylV-x4AS-Ok74rBVTtGh4yYbZkcB9DKzlrefC8pCGDZxt4Qpkf8h1QL-8U33Z0gtZVnv6rO9NHF91aLb1_ED-OxeSvGjbmw-iEfZF",
+        notification: {
+          body: body,
+          title: title,
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/fooswarriors-bdc5e.appspot.com/o/Logo512.png?alt=media&token=204a88c6-651f-42e7-8fd3-7782af799c38",
+        },
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "key=AAAALB8TIAw:APA91bEnaOmac-vjC-UzV9IMUq4miRuombGUCEMVjeghbI9LNCaN2YzW2ONZZ2hgPYzizptOPhHUjsvf37-0kEKPWEiNCHCCn7VEFLn-qWqzkmHMlRUfE5HBgHIjkb_u96pi4QuAXKlN",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log("result is: ", JSON.stringify(result));
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log("error message: ", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error: ", error);
+      return "An unexpected error occurred";
+    }
+  }
+};
--- src/views/landing-page/LandingPage.tsx
+++ src/views/landing-page/LandingPage.tsx
@@ -3,10 +3,22 @@ import Logo from "@/assets/logos/logos";
 import { PrimaryButton } from "@/components/primary-button/PrimaryButton";
 import { useUser } from "@/context/UserContext";
 import "./LandingPage.sass";
+import { sendMessage } from "@/utils/hooks";
 
 export const LandingPage = () => {
   const navigate = useNavigate();
   const { isLoggedIn } = useUser();
+
+  const testMessage = {
+    to: "f7IhuiPrpWx7ev9yo2xBaX:APA91bHEtDxR1KLytreym1rziQ-9CTBZBp7RS7zylV-x4AS-Ok74rBVTtGh4yYbZkcB9DKzlrefC8pCGDZxt4Qpkf8h1QL-8U33Z0gtZVnv6rO9NHF91aLb1_ED-OxeSvGjbmw-iEfZF",
+    notification: {
+      body: "Firebase Cloud Message Body",
+      title: "firebase cloud message title",
+    },
+  };
+  const test = () => {
+    sendMessage({ body: "hej", title: "tjo" });
+  };
 
   return (
     <div className="landingPage">
@@ -23,6 +35,7 @@ export const LandingPage = () => {
           secondary
           title="Enter Battlefield"
         />
+        <PrimaryButton onClick={test} secondary title="Enter Battlefield" />
       </div>
     </div>
   );
