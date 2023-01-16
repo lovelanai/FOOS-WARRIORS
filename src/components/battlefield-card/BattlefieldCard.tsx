a0ddf002d9cac9ecc9338c4f8e6530dcc2cfbdf7
Love Lanai <love.lanai@medieinstitutet.se>
2023-01-16 14:08:03 +0100

    fixed desktop for navigation

--- src/components/battlefield-card/BattlefieldCard.tsx
+++ src/components/battlefield-card/BattlefieldCard.tsx
@@ -11,58 +11,6 @@ export interface TeamProps {
   winners?: string;
 }
 
-export const BattlefieldCard = ({
-  playerOne,
-  playerTwo,
-  playerThree,
-  playerFour,
-}: TeamProps) => {
-  return (
-    <div className="battleCard">
-      <div className="pinkTeam">
-        <div
-          className="player"
-          style={{ backgroundImage: `url(${playerOne.img})` }}
-        >
-          <div className="overlay">
-            <h4 className="name">{playerOne.name}</h4>
-          </div>
-        </div>
-        <div
-          className="player"
-          style={{ backgroundImage: `url(${playerTwo.img})` }}
-        >
-          <div className="overlay">
-            <h4 className="name">{playerTwo.name}</h4>
-          </div>
-        </div>
-      </div>
-      <div className="matchContent">
-        <Logo.Ball />
-        <h3 className="title">VS</h3>
-      </div>
-      <div className="redTeam">
-        <div
-          className="player"
-          style={{ backgroundImage: `url(${playerThree.img})` }}
-        >
-          <div className="overlay">
-            <h4 className="name">{playerThree.name}</h4>
-          </div>
-        </div>
-        <div
-          className="player"
-          style={{ backgroundImage: `url(${playerFour.img})` }}
-        >
-          <div className="overlay">
-            <h4 className="name">{playerFour.name}</h4>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
 export const BattlefieldWinnerCard = ({
   playerOne,
   playerTwo,
