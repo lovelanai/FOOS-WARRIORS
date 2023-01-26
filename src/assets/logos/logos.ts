f49370626202c88ae9c218a04f68e8e0f2a7262e
Love Lanai <love.lanai@medieinstitutet.se>
2023-01-26 13:07:20 +0100

    removed unused icons

--- src/assets/logos/logos.ts
+++ src/assets/logos/logos.ts
@@ -1,14 +1,12 @@
-import { ReactComponent as Player } from "./player.svg";
-import { ReactComponent as BigPlayer } from "./bigPlayer.svg";
-import { ReactComponent as Swords } from "./swords.svg";
 import { ReactComponent as Ball } from "./ball.svg";
-import { ReactComponent as PlayerOnField } from "./playerOnField.svg";
+import { ReactComponent as BattleSkull } from "./battleSkull.svg";
+import { ReactComponent as BigPlayer } from "./bigPlayer.svg";
 import { ReactComponent as HiQ } from "./hiq.svg";
 import { ReactComponent as Offline } from "./offlineLogo.svg";
-import { ReactComponent as BattleSkull } from "./battleSkull.svg";
+import { ReactComponent as PlayerOnField } from "./playerOnField.svg";
+import { ReactComponent as Swords } from "./swords.svg";
 
 const Logo = {
-  Player,
   BigPlayer,
   Swords,
   Ball,
